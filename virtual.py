9365e909e2fb9097c4b0c533834dece8c5f1edce
lbosch <lbosch@icra.cat>
2016-10-04 01:08:08 +0200

    afegit virtual.Serial.flush()

--- virtual.py
+++ virtual.py
@@ -7,12 +7,6 @@ class Serial:
 	Trama = "IT1-300,T2-350,T3-375,T4-450,P1-180,P2-200,P3-400,P4-700,C1-0,C2-0,C3-0,C4-0F" #no canvia mai
 	trama = Trama
 	port ="VIRTUAL"
-	def flushInput(self):
-		pass
-	def isOpen(self):
-		return True
-	def write(self,ordre):
-		print "Has enviat %s" % ordre
 	def read(self):
 		n=len(self.trama)
 		if n is 0: 
@@ -21,6 +15,10 @@ class Serial:
 		c=self.trama[0]
 		self.trama=self.trama[1:n]
 		return c
+	def flush(self): pass
+	def flushInput(self): pass
+	def isOpen(self): return True
+	def write(self,ordre): print "Has enviat %s" % ordre
 #test
 '''
 ser = Serial()    

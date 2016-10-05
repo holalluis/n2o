37301004a632b8958e06589a235bb2f14d73805d
lbosch <lbosch@icra.cat>
2016-10-05 17:56:28 +0100

    errors corregits i test comptar volum durant espera

--- sessio.py
+++ sessio.py
@@ -18,6 +18,12 @@ if len(sys.argv)!=2:
 else:
 	tempsEspera=sys.argv[1]
 
+#transforma a enter
+tempsEspera=int(tempsEspera)
+
+#comprova tempsEspera
+if(tempsEspera<0): raise RuntimeError("temps espera negatiu")
+
 os.system('clear')
 print("Adquirint dades cada %s segons" %tempsEspera)
 
@@ -46,39 +52,56 @@ C1=2;C2=2;C3=2;C4=2
 
 #comença bucle de lectura
 trama=""
+lectures=0 #nombre de lectures fetes mentre esperes (1 segon)
 while True:
-    c=ser.read()
-    trama+=c
-    if c is "F":
-        try: 
-            d=Pro.processa(trama) #saltarà si la trama és incorrecta
-            trama=""
-        except: 
-            trama=""
-            continue
-    else:
-        continue #continua afegint caracters a la trama
 
-    #si som aquí vol dir que la trama és correcta
-    #si l'estat del pols ha canviat, suma 10 L
-    if(d['C1']!=C1): V1+=10
-    if(d['C2']!=C2): V2+=10
-    if(d['C3']!=C3): V3+=10
-    if(d['C4']!=C4): V4+=10
-    #update estat del pols
-    C1=d['C1'];C2=d['C2'];C3=d['C3'];C4=d['C4'];
-    #afegeix el volum a l'objecte "d"
-    d['V1']=V1;d['V2']=V2;d['V3']=V3;d['V4']=V4
-    #registra a la base de dades
-    try:
-	   Reg.registra(d)
-    except:
-	   print("Dades no insertades")
-    #esperem X temps
-    time.sleep(float(tempsEspera))
+    #ves comptant el volum mentre esperes
+    while True:
+        c=ser.read()
+        trama+=c
+        if c is "F":
+            try: 
+                d=Pro.processa(trama) #saltarà si la trama és incorrecta
+                for i in range(4): sys.stdout.write("\033[F\033[K")
+                trama=""
+            except: 
+                print trama
+                trama=""
+                ser.flushInput()
+                continue
+        else:
+            continue #continua afegint caracters a la trama
+
+        #si som aquí vol dir que la trama és correcta
+        #si l'estat del pols ha canviat, suma 10 L
+        if(d['C1']!=C1): V1+=10
+        if(d['C2']!=C2): V2+=10
+        if(d['C3']!=C3): V3+=10
+        if(d['C4']!=C4): V4+=10
+        #update estat del pols
+        C1=d['C1'];C2=d['C2'];C3=d['C3'];C4=d['C4'];
+        #afegeix el volum a l'objecte "d"
+        d['V1']=V1;d['V2']=V2;d['V3']=V3;d['V4']=V4
+
+        sys.stdout.write("\033[F\033[K")
+        print "Volum (L): "+str([V1,V2,V3,V4])
+
+        #registra a la base de dades si el comptador està a zero
+        if lectures is 0:
+            try: Reg.registra(d)
+            except: print("Error: Dades no registrades")
+
+        lectures+=1
+        time.sleep(1)
+        if lectures > tempsEspera: break
+        ser.flushInput()
+    #final del temps d'espera
+
+    #reset comptador de lectures
+    lectures=0
     #neteja pantalla
     os.system('clear')
     print("Adquirint dades cada %s segons" %tempsEspera)
-	#flush input! IMPORTANTISSIM
+    #flush input! IMPORTANTISSIM
     ser.flushInput()
 
--- virtual.py
+++ virtual.py
@@ -4,7 +4,7 @@
 '''
 
 class Serial:
-	Trama = "IT1-300,T2-350,T3-375,T4-450,P1-180,P2-200,P3-400,P4-700,C1-0,C2-0,C3-0,C4-0,E1-1,E2-0,E3-0,E4-0F" #no canvia mai
+        Trama = "IT1-300,T2-350,T3-375,T4-450,P1-180,P2-200,P3-400,P4-700,E1-1,E2-0,E3-0,E4-0,C1-0,C2-0,C3-0,C4-0F" #no canvia mai
 	trama = Trama
 	port ="VIRTUAL"
 	def read(self):
